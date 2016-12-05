e161c1b5efabe975116b7537eccf84f3e9417fc4
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-05 14:14:49 -0600

    Color boxes functioning

--- client.js
+++ client.js
@@ -1,3 +1,23 @@
+var colorTotals = {};
+
 $(document).ready(function() {
-  console.log('jq');
+    colorTotalSetup();
+    enableButtons();
 });
+
+function enableButtons() {
+    $(document).on('click', '.color-button', colorClicked);
+}
+
+function colorClicked() {
+    var color = $(this).data('color');
+    $('.container').append('<div class="color-cube ' + color + '"></div>');
+    colorTotals[color]++;
+    $('#' + color).text('Total ' + color + ': ' + colorTotals[color]);
+}
+
+function colorTotalSetup() {
+    $('.color-button').each(function(button) {
+        colorTotals[$(this).data('color')] = 0;
+    });
+}
